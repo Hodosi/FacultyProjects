165a568985082ab971d4c0efd3586197b0d26e1d
Attila <hodosi.attila@yahoo.com>
2021-03-05 12:17:51 +0200

    Project init

---
+++ ImmobileAdministrator/cmake-build-debug/CMakeFiles/ImmobileAdministrator.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ImmobileAdministrator.dir/main.c.obj"
+  "ImmobileAdministrator.exe"
+  "ImmobileAdministrator.exe.manifest"
+  "ImmobileAdministrator.pdb"
+  "libImmobileAdministrator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ImmobileAdministrator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
