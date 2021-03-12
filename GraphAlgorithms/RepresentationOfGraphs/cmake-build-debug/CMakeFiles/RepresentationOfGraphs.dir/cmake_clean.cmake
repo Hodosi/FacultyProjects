0ebbabdf8c5b87972fdc47e287f8f9254c48dd22
Attila <hodosi.attila@yahoo.com>
2021-03-12 12:43:29 +0200

    Create GraphAlgorithms folder

---
+++ GraphAlgorithms/RepresentationOfGraphs/cmake-build-debug/CMakeFiles/RepresentationOfGraphs.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RepresentationOfGraphs.dir/main.cpp.obj"
+  "RepresentationOfGraphs.exe"
+  "RepresentationOfGraphs.exe.manifest"
+  "RepresentationOfGraphs.pdb"
+  "libRepresentationOfGraphs.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RepresentationOfGraphs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
